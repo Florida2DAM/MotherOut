450369e6e14dd7fdb9989bd74e8c44e651d7f873
JFGalan <71388943+JFGalan@users.noreply.github.com>
2021-02-03 19:31:23 +0100

    "Subimos otra vez la rama con las cases repository de Icons, Tems y PredefinedTasks. Asimismo, también los controllers."

---
+++ BackEnd/MotherOut/MotherOut/Controllers/IconsController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Drawing;
+using MotherOut_BackEnd.Models;
+
+
+namespace MotherOut_BackEnd.Controllers
+{
+    public class IconsController : ApiController
+    {
+        // GET: api/Icons
+        public IEnumerable<Image> Get()
+        {
+            var repo = new IconsRepository();
+            List<Image> icons = repo.showIcons();
+            return icons;
+        }
+
+        // GET: api/Icons/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Icons
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Icons/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Icons/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ BackEnd/MotherOut/MotherOut/Controllers/PreDefinedTasksController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using MotherOut_BackEnd.Models;
+
+namespace MotherOut_BackEnd.Controllers
+{
+    public class PreDefinedTasksController : ApiController
+    {
+        // GET: api/PreDefinedTasks
+        public IEnumerable<PreDefinedTask> Get()
+        {
+            var repo = new PreDefinedTaskRepository();
+            List<PreDefinedTask> preDefinedTasks = repo.getPreDefinedTasks();
+            return preDefinedTasks;
+        }
+
+        // GET: api/PreDefinedTasks/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/PreDefinedTasks
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/PreDefinedTasks/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/PreDefinedTasks/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
