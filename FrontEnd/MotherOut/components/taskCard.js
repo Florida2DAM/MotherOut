cc63bf65fd07588cc03a9d78934c5c935888388b
Ferriol-blip <70396199+Ferriol-blip@users.noreply.github.com>
2021-02-05 20:26:29 +0100

    Card modficiada de task. Casi terminada card de stadisticas

---
+++ FrontEnd/MotherOut/components/taskCard.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import {
+    StyleSheet,
+    View,
+    Text,
+    Image
+} from 'react-native';
+
+import {
+    Icon,
+} from "react-native-elements"
+
+//Se le pasará un icono de la base de datos, dejo este codigo como ejemplo para otros imports locales
+import logo from '../assets/house_bed_icon-icons.com_74373.png'
+const defaultLogo = Image.resolveAssetSource(logo).uri;
+
+export class CardTaskbox extends Component {
+
+    render() {
+        return (
+            <>
+                <View style={styles.cardTaskbox}>
+                    <Image
+                        style={styles.logo}
+                        source={{ uri: defaultLogo }}
+                    />
+                    <Text style={styles.textStyle}>{this.props.text}</Text>
+                    <Icon
+                        name={this.props.icon}
+                        type='font-awesome'
+                        color='#F4CAE0'
+                        onPress={this.props.press}
+                        size={50} />
+                </View>
+            </>
+        );
+    }
+
+}
+
+const styles = StyleSheet.create({
+    cardTaskbox: {
+        flexDirection: "row",
+        justifyContent: "space-evenly",
+        backgroundColor: "#ADA7C9",
+        borderRadius: 5,
+        borderWidth: 2,
+        borderColor: "#64A6BD",
+        //Shadow
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 12,
+        },
+        shadowOpacity: 0.58,
+        shadowRadius: 16.00,
+        elevation: 24,
+    },
+    textStyle: {
+        padding: 10,
+        fontSize: 25,
+        fontFamily: "Roboto",
+        fontWeight: "bold"
+    },
+    logo: {
+        width: 66,
+        height: 58,
+    },
+});
