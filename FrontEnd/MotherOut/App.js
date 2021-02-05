d7d732b8fcc721af2f58aefd06ed1f992cd5171d
juan_lujan <juanilloxyz@hotmail.com>
2021-02-05 19:15:43 +0100

    termino input relleno

--- FrontEnd/MotherOut/App.js
+++ FrontEnd/MotherOut/App.js
@@ -11,37 +11,56 @@ import {StyleSheet, Text, View} from 'react-native';
 
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 import {GenericInput1} from './components/GenericInput1';
+import {GenericInput2} from './components/GenericInput2';
 
 class App extends Component {
-constructor(props) {
-    super(props);
-    this.state={
-        email : null,
-        password:null
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: null,
+            password: null,
+            taskName: null,
+        };
+
     }
 
-}
-    changeEmail(text){
-    this.setState({email: text});
-}
-    changePassword(text){
-    this.setState({password: text});
+    changeEmail(text) {
+        this.setState({email: text});
     }
+
+    changePassword(text) {
+        this.setState({password: text});
+    }
+
+    changeTask(text) {
+        this.setState({taskName: text});
+    }
+
     render() {
         return (
             <>
                 <View style={styles.backGround}>
                     <View style={{flex: 1, backgroundColor: 'pink'}}></View>
                     <View style={{flex: 1}}>
-                        <GenericInput1 placeHolder={'Email'} passValue={false} value={this.state.email} onChange={text => this.changeEmail(text) }/>
-                        <GenericInput1 placeHolder={'Password'} passValue={true} value={this.state.password} onChange={text=>this.changePassword(text)}/>
+                        <GenericInput1 placeHolder={'Email'} passValue={false} value={this.state.email}
+                                       onChange={text => this.changeEmail(text)}/>
+                        <GenericInput1 placeHolder={'Password'} passValue={true} value={this.state.password}
+                                       onChange={text => this.changePassword(text)}/>
                         <Text>{this.state.password}</Text>
                     </View>
-                    <View style={{flex: 1, backgroundColor: 'grey'}}></View>
+                    <View style={{flex: 1, backgroundColor: 'grey'}}>
+                        <Text>Task name</Text>
+                        <GenericInput2 placeHolder={'Task name'} passValue={false} value={this.state.taskName}
+                                       onChange={text => this.changeTask(text)}/>
+                        <Text>{this.state.taskName}</Text>
+                        <Text>Select member</Text>
+                        <Text>Select day</Text>
+                    </View>
                 </View>
             </>
         );
     }
+
 }
 
 const styles = StyleSheet.create({
