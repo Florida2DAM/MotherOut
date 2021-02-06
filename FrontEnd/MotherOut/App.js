bf2cf2e9cd8677f91ffef72c2a4d3a70cb82db27
juan_lujan <juanilloxyz@hotmail.com>
2021-02-06 10:49:15 +0100

    noTicket

--- FrontEnd/MotherOut/App.js
+++ FrontEnd/MotherOut/App.js
@@ -7,56 +7,69 @@
  */
 
 import React, {Component} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {
+    SafeAreaView,
+    StyleSheet,
+    ScrollView,
+    View,
+    Text,
+    StatusBar,
+} from 'react-native';
 
-import {Colors} from 'react-native/Libraries/NewAppScreen';
-import {GenericInput1} from './components/GenericInput1';
-import {GenericInput2} from './components/GenericInput2';
+import {
+    Header,
+    LearnMoreLinks,
+    Colors,
+    DebugInstructions,
+    ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            email: null,
-            password: null,
-            taskName: null,
-        };
-
-    }
-
-    changeEmail(text) {
-        this.setState({email: text});
-    }
-
-    changePassword(text) {
-        this.setState({password: text});
-    }
-
-    changeTask(text) {
-        this.setState({taskName: text});
-    }
 
     render() {
         return (
             <>
-                <View style={styles.backGround}>
-                    <View style={{flex: 1, backgroundColor: 'pink'}}></View>
-                    <View style={{flex: 1}}>
-                        <GenericInput1 placeHolder={'Email'} passValue={false} value={this.state.email}
-                                       onChange={text => this.changeEmail(text)}/>
-                        <GenericInput1 placeHolder={'Password'} passValue={true} value={this.state.password}
-                                       onChange={text => this.changePassword(text)}/>
-                        <Text>{this.state.password}</Text>
-                    </View>
-                    <View style={{flex: 1, backgroundColor: 'grey'}}>
-                        <Text>Task name</Text>
-                        <GenericInput2 placeHolder={'Task name'} passValue={false} value={this.state.taskName}
-                                       onChange={text => this.changeTask(text)}/>
-                        <Text>{this.state.taskName}</Text>
-                        <Text>Select member</Text>
-                        <Text>Select day</Text>
-                    </View>
-                </View>
+                <StatusBar barStyle="dark-content"/>
+                <SafeAreaView>
+                    <ScrollView
+                        contentInsetAdjustmentBehavior="automatic"
+                        style={styles.scrollView}>
+                        <Header/>
+                        {global.HermesInternal == null ? null : (
+                            <View style={styles.engine}>
+                                <Text style={styles.footer}>Engine: Hermes</Text>
+                            </View>
+                        )}
+                        <View style={styles.body}>
+                            <View style={styles.sectionContainer}>
+                                <Text style={styles.sectionTitle}>Step One</Text>
+                                <Text style={styles.sectionDescription}>
+                                    Edit <Text style={styles.highlight}>App.js</Text> to change this
+                                    screen and then come back to see your edits.
+                                </Text>
+                            </View>
+                            <View style={styles.sectionContainer}>
+                                <Text style={styles.sectionTitle}>See Your Changes</Text>
+                                <Text style={styles.sectionDescription}>
+                                    <ReloadInstructions/>
+                                </Text>
+                            </View>
+                            <View style={styles.sectionContainer}>
+                                <Text style={styles.sectionTitle}>Debug</Text>
+                                <Text style={styles.sectionDescription}>
+                                    <DebugInstructions/>
+                                </Text>
+                            </View>
+                            <View style={styles.sectionContainer}>
+                                <Text style={styles.sectionTitle}>Learn More</Text>
+                                <Text style={styles.sectionDescription}>
+                                    Read the docs to discover what to do next:
+                                </Text>
+                            </View>
+                            <LearnMoreLinks/>
+                        </View>
+                    </ScrollView>
+                </SafeAreaView>
             </>
         );
     }
@@ -64,11 +77,6 @@ class App extends Component {
 }
 
 const styles = StyleSheet.create({
-    backGround: {
-        flex: 1,
-        flexDirection: 'column',
-        backgroundColor: '#90A8C3',
-    },
     scrollView: {
         backgroundColor: Colors.lighter,
     },
