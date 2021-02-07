24181e4280e8fc9c03011758cf512dec465c8b23
JFGalan <71388943+JFGalan@users.noreply.github.com>
2021-02-07 20:03:27 +0100

    "Cambios introducidos en las pantallas de la aplicación, limpieza y eficiencia de código."

--- FrontEnd/MotherOut/components/screens/AsignedTasks.js
+++ FrontEnd/MotherOut/components/screens/AsignedTasks.js
@@ -1,18 +1,12 @@
-
-import React, { Component } from 'react';
-import {
-    FlatList, StyleSheet,
-    Text, View
-} from 'react-native';
-import {
-    Image
-} from 'react-native-elements';
+import React, {Component} from 'react';
+import {FlatList, StyleSheet, Text, View} from 'react-native';
+import {Image} from 'react-native-elements';
 import importedPicture from '../../assets/asignedTasks.png';
 import importAvatar2 from '../../assets/avatar2.png';
 import importIcon from '../../assets/bathtub.png';
 import importAvatar from '../../assets/circle-cropped.png';
-import { NavBar } from '../NavBar';
-import { TaskCardTwoIcons } from '../TaskCardTwoIcons';
+import {NavBar} from '../NavBar';
+import {TaskCardTwoIcons} from '../TaskCardTwoIcons';
 
 const avatar = Image.resolveAssetSource(importAvatar).uri
 const avatar2 = Image.resolveAssetSource(importAvatar2).uri
@@ -21,20 +15,15 @@ const picture = Image.resolveAssetSource(importedPicture).uri;
 
 
 const taskList = [
-    { task: "Clean Bathroom", blop: icon },
-    { task: "Clean Bathroom", blop: icon },
-    { task: "Clean Bathroom", blop: icon },
-    { task: "Clean Bathroom", blop: icon },
+    {task: "Clean Bathroom", blop: icon},
+    {task: "Clean Bathroom", blop: icon},
+    {task: "Clean Bathroom", blop: icon},
+    {task: "Clean Bathroom", blop: icon},
 ];
 
 const listUsers = [
-    { name: 'Pablo', blop: avatar2 }, { name: 'Juan', blop: avatar }, { name: 'Jesus', blop: avatar }
+    {name: 'Pablo', blop: avatar2}, {name: 'Juan', blop: avatar}, {name: 'Jesus', blop: avatar}
 ]
-
-import imagen from '../../assets/asignedTasks.png';
-import { GenericIconButton } from '../GenericIconButton';
-import { NavBar } from '../NavBar';
-
 
 class AsignedTasks extends Component {
 
@@ -44,7 +33,6 @@ class AsignedTasks extends Component {
             task: null,
         }
     }
-
 
 
     getTask = (itemSelected) => {
@@ -57,31 +45,31 @@ class AsignedTasks extends Component {
                 <View style={styles.contenidor}>
                     <View style={styles.header}>
                         <Image
-                            style={{ width: 300, height: 90 }}
-                            source={{ uri: picture }} />
+                            style={{width: 300, height: 90}}
+                            source={{uri: picture}}/>
                     </View>
                     <View style={styles.body}>
                         <FlatList
                             data={listUsers}
                             keyExtractor={(item, index) => index.toString()}
-                            renderItem={({ item }) =>
+                            renderItem={({item}) =>
                                 <View>
                                     <View style={styles.headUser}>
                                         <Text style={styles.textStyle}>{item.name}</Text>
                                         <Image
                                             style={styles.logo}
-                                            source={{ uri: item.blop }}
+                                            source={{uri: item.blop}}
                                         />
                                     </View>
                                     <FlatList
                                         data={taskList}
                                         keyExtractor={(item, index) => index.toString()}
-                                        renderItem={({ item }) =>
+                                        renderItem={({item}) =>
                                             <View style={styles.flatStyle}>
                                                 <TaskCardTwoIcons text={item.task}
-                                                    icon1="trash"
-                                                    icon2="edit"
-                                                    iconCard={item.blop} />
+                                                                  icon1="trash"
+                                                                  icon2="edit"
+                                                                  iconCard={item.blop}/>
                                             </View>
                                         }
                                     />
@@ -91,7 +79,7 @@ class AsignedTasks extends Component {
 
                     </View>
                     <View>
-                        <NavBar />
+                        <NavBar/>
                     </View>
                 </View>
             </>
