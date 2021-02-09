fb68b0548e4617a082a330cc91aa2129b0279b55
juan_lujan <juanilloxyz@hotmail.com>
2021-02-09 20:10:48 +0100

    completo consulta axios de Statistics

--- FrontEnd/MotherOut/components/screens/Statistics.js
+++ FrontEnd/MotherOut/components/screens/Statistics.js
@@ -1,9 +1,10 @@
-import React, { Component } from 'react';
-import { FlatList, StyleSheet, View } from 'react-native';
-import { Image } from 'react-native-elements';
+import React, {Component} from 'react';
+import {FlatList, StyleSheet, Text, View} from 'react-native';
+import {Image} from 'react-native-elements';
 import imagen from '../../assets/statistics.png';
-import { NavBar } from '../NavBar';
-import { StatisticCard } from '../StatisticCard';
+import {NavBar} from '../NavBar';
+import {StatisticCard} from '../StatisticCard';
+import axios from 'axios';
 
 const picture = Image.resolveAssetSource(imagen).uri;
 
@@ -11,30 +12,57 @@ class Statistics extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            data: [{ user: 'Animus58', quantity: 15, Score: 230 },
-            { user: 'Ambros77', quantity: 7, Score: 77 },
-            { user: 'Arnald12', quantity: 12, Score: 200 },
-            ],
+            idTeam: null,
+            teamData: [],
+            teamUserData: [],
         };
     }
+
+    componentDidMount() {
+        //recuperar los valores que me mandar desde otra pagina.
+        //const idUser = this.props.route.params.userId;
+        const idUser = 4;
+        this.getIdTeam(idUser);
+
+    }
+
+    getUserByTeam(idTeam) {
+        axios.get('http://52.0.146.162:80/api/Users?idTeam=' + idTeam).then(response => {
+            this.setState({teamData: response.data});
+        })
+            .catch(function (error) {
+                alert(error);
+            });
+    };
+
+    getIdTeam(idUser) {
+        axios.get('http://52.0.146.162:80/api/Users?idUser=' + idUser).then(response => {
+            this.setState({idTeam: response.data.AsignedTeam});
+            this.getUserByTeam(this.state.idTeam);
+        })
+            .catch(function (error) {
+                alert(error);
+            });
+    }
+
     render() {
         return (
             <>
                 <View style={styles.contenidor}>
                     <View style={styles.header}>
                         <Image
-                            style={{ width: 300, height: 90 }}
-                            source={{ uri: picture }}
+                            style={{width: 300, height: 90}}
+                            source={{uri: picture}}
                         />
                     </View>
                     <View style={styles.body}>
-                        <FlatList data={this.state.data}
-                            keyExtractor={(item, index) => index.toString()}
-                            renderItem={({ item }) => (
-                                <View style={{ padding: 10 }}>
-                                    <StatisticCard user={item.user} quantity={item.quantity}
-                                        score={item.Score} />
-                                </View>)}
+                        <FlatList data={this.state.teamData}
+                                  keyExtractor={(item, index) => index.toString()}
+                                  renderItem={({item}) => (
+                                      <View style={{padding: 10}}>
+                                          <StatisticCard user={item.Name} quantity={item.NTaks}
+                                                         score={item.UserScore}/>
+                                      </View>)}
                         />
                     </View>
                     <View>
