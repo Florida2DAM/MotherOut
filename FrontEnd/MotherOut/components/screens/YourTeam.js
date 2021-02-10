d7da0cea9980ca2a11486a863ca504fe401c6523
juan_lujan <juanilloxyz@hotmail.com>
2021-02-10 18:06:15 +0100

    edito nombre de equipo y elimino equipo con axios

--- FrontEnd/MotherOut/components/screens/YourTeam.js
+++ FrontEnd/MotherOut/components/screens/YourTeam.js
@@ -8,6 +8,7 @@ import {RoundedButton} from '../RoundedButton';
 import {RoundedButton2} from '../RoundedButton2';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import axios from 'axios';
+import item from 'react-native-calendars/src/calendar-list/item';
 
 const picture = Image.resolveAssetSource(imagen).uri;
 
@@ -20,7 +21,8 @@ class YourTeam extends Component {
             user: [],
             teamData: [],
             nameTeam: null,
-            hola:'hola',
+            newNameTeam: null,
+            hola: 'hola',
         };
     }
 
@@ -34,70 +36,97 @@ class YourTeam extends Component {
             });
     };
 
-    async getData() {
+    getData = async () => {
         try {
             const jsonValue = await AsyncStorage.getItem('logUser');
             jsonValue != null ? this.setState({user: JSON.parse(jsonValue)}) : null;
         } catch (e) {
             alert(e);
         }
-    }
+    };
 
-    getUserByTeam(idTeam) {
+    getUserByTeam = (idTeam) => {
         axios.get('http://52.0.146.162:80/api/Users?idTeam=' + idTeam).then(response => {
             this.setState({teamData: response.data});
         })
             .catch(function (error) {
                 alert(error);
             });
-    }
+    };
 
-    getTeamName(idTeam) {
+    getTeamName = (idTeam) => {
         axios.get('http://52.0.146.162:80/api/Teams?idTeam=' + idTeam).then(response => {
             this.setState({nameTeam: response.data});
-            alert(this.state.nameTeam);
         })
             .catch(function (error) {
                 alert(error);
             });
-    }
+    };
 
+    deleteTeam = () => {
+        axios.delete('http://52.0.146.162:80/api/Teams?idTeam='+this.state.user.AsignedTeam).then(response=>{
+            if(response.data===true){
+                alert("your team is history");
+            }
+            else {
+                alert("not delete the team");
+            }
+        });
+    };
+
+    putNameTeam = () => {
+        axios.put('http://52.0.146.162:80/api/Teams?idTeam='+this.state.user.AsignedTeam+'&newTeamName='+this.state.newNameTeam).then(response=>{
+            if (response.data==true){
+                alert("Your new name team is changed");
+            }
+            else{
+                alert("not change your name team");
+            }
+        })
+
+    };
 
     render() {
         return (
             <>
                 <View style={styles.contenidor}>
-                    <ScrollView>
-                        <View style={styles.header}>
-                            <Image
-                                style={{width: 300, height: 90}}
-                                source={{uri: picture}}/>
-                        </View>
-                        <View style={styles.body}>
-                            <View style={styles.garbage}>
-                                <Text style={styles.text}>Group name</Text>
-                                <RoundedButton2 icon={'trash'}/>
-                            </View>
-                            <GenericInput3 placeHolder={this.state.nameTeam}/>
-                            <View>
-                                <Text style={styles.text}>Members</Text>
-                            </View>
-                            <View style={{marginTop: 18}}>
-                                <FlatList data={this.state.teamData}
-                                          keyExtractor={(item, index) => index.toString()}
-                                          renderItem={({item}) =>
-                                              <View style={styles.userBox}>
-                                                  <Pressable>
-                                                      <Text style={styles.textStyle}>{item.Name}</Text>
-                                                  </Pressable>
-                                              </View>}
-                                />
+
+                    <View style={styles.header}>
+                        <Image
+                            style={{width: 300, height: 90}}
+                            source={{uri: picture}}/>
+                    </View>
+                    <View style={styles.body}>
+                        <View style={styles.garbage}>
+                            <Text style={styles.text}>Group name</Text>
+                            <View style={{flexDirection: 'row'}}>
+                                <RoundedButton2 icon={'edit'} press={this.putNameTeam}/>
+                                <RoundedButton2 icon={'trash'} press={this.deleteTeam}/>
                             </View>
                         </View>
                         <View>
-                            <RoundedButton icon="plus"/>
+                            <GenericInput3 placeHolder={this.state.nameTeam} value={this.state.newNameTeam}
+                                           onChange={(item) => this.setState({newNameTeam: item})}/>
                         </View>
-                    </ScrollView>
+                        <View>
+                            <Text style={styles.text}>Members</Text>
+                        </View>
+                        <View style={{marginTop: 18}}>
+                            <FlatList data={this.state.teamData}
+                                      keyExtractor={(item, index) => index.toString()}
+                                      renderItem={({item}) =>
+                                          <View style={styles.userBox}>
+                                              <Pressable>
+                                                  <Text style={styles.textStyle}>{item.Name}</Text>
+                                              </Pressable>
+                                          </View>}
+                            />
+                        </View>
+                    </View>
+                    <View>
+                        <RoundedButton icon="plus"/>
+                    </View>
+
                     <View>
                         <NavBar
                             checked={() => this.props.navigation.navigate('ScreenToDo')}
