f77562a97be92791dee284ab58a544cbd0d0c5e7
JFGalan <71388943+JFGalan@users.noreply.github.com>
2021-02-11 17:52:04 +0100

    "Implementación de los Toast en SingUp.js"

--- FrontEnd/MotherOut/components/screens/SingUp.js
+++ FrontEnd/MotherOut/components/screens/SingUp.js
@@ -1,17 +1,18 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
     ScrollView, StyleSheet,
-    View, Text, Linking
+    View, Text, Linking, ToastAndroid
 } from 'react-native';
 import {
     Image
 } from 'react-native-elements';
 import imagen from '../../assets/logo.png';
-import { GenericButton } from '../GenericButton';
-import { GenericInput1 } from '../GenericInput1';
-import { CheckBox } from 'react-native-elements'
+import {GenericButton} from '../GenericButton';
+import {GenericInput1} from '../GenericInput1';
+import {CheckBox} from 'react-native-elements'
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+
 const picture = Image.resolveAssetSource(imagen).uri;
 
 class SingUp extends Component {
@@ -31,14 +32,18 @@ class SingUp extends Component {
             const jsonValue = JSON.stringify(res)
             await AsyncStorage.setItem('logUser', jsonValue)
         } catch (e) {
-            alert(e)
+            ToastAndroid.showWithGravityAndOffset(e, ToastAndroid.LONG,
+                ToastAndroid.TOP,
+                25,
+                50);
         }
     }
+
     //no me queda claro
     componentWillUnmount() {
-        this.setState({ name: null })
-        this.setState({ email: null })
-        this.setState({ password: null })
+        this.setState({name: null})
+        this.setState({email: null})
+        this.setState({password: null})
     }
 
     addUser = () => {
@@ -49,22 +54,31 @@ class SingUp extends Component {
             Help: true
         }
 
-        if (this.state.name != ''  || this.state.email != '' || this.state.password != '') {
+        if (this.state.name != '' || this.state.email != '' || this.state.password != '') {
             if (this.state.checked) {
                 axios.post('http://52.0.146.162:80/api/Users', user)
                     .then(() => {
                         this.storeData(user);
-                        alert("Peticion enviada")
+                        ToastAndroid.showWithGravityAndOffset("It has been successfully registered. You will now be able to join a herd pigs.", ToastAndroid.LONG,
+                            ToastAndroid.TOP,
+                            25,
+                            50)
                     })
                     .catch((error) => {
                         alert(error);
                     });
                 this.props.navigation.navigate('Help1')
             } else {
-                alert("Acepta los términos para poder vender tus datos")
+                ToastAndroid.showWithGravityAndOffset("You must agree to the terms in order to sell them, little piggy.", ToastAndroid.LONG,
+                    ToastAndroid.TOP,
+                    25,
+                    50);
             }
         } else {
-            alert("Campos vacios")
+            ToastAndroid.showWithGravityAndOffset("There are empty fields, piggy. You must fill them all in.", ToastAndroid.LONG,
+                ToastAndroid.TOP,
+                25,
+                50);
         }
     }
 
@@ -75,29 +89,32 @@ class SingUp extends Component {
                     <ScrollView>
                         <View style={styles.pictures}>
                             <Image
-                                style={{ width: 310, height: 270 }}
-                                source={{ uri: picture }}
+                                style={{width: 310, height: 270}}
+                                source={{uri: picture}}
                             />
                         </View>
                         <View style={styles.inputs}>
-                            <GenericInput1 placeHolder="Name" value={this.state.name} onChange={(item) => this.setState({ name: item })} />
-                            <GenericInput1 placeHolder="Email" value={this.state.email} onChange={(item) => this.setState({ email: item })} />
-                            <GenericInput1 placeHolder="Pasword" value={this.state.passwordl} passValue={true} onChange={(item) => this.setState({ password: item })} />
+                            <GenericInput1 placeHolder="Name" value={this.state.name}
+                                           onChange={(item) => this.setState({name: item})}/>
+                            <GenericInput1 placeHolder="Email" value={this.state.email}
+                                           onChange={(item) => this.setState({email: item})}/>
+                            <GenericInput1 placeHolder="Pasword" value={this.state.passwordl} passValue={true}
+                                           onChange={(item) => this.setState({password: item})}/>
                             <CheckBox
                                 containerStyle={styles.check}
                                 checkedIcon={'check-square'}
                                 uncheckedIcon={'square'}
                                 checked={this.state.checked}
-                                onPress={() => this.setState({ checked: !this.state.checked })}
+                                onPress={() => this.setState({checked: !this.state.checked})}
                                 title={
                                     <Text style={styles.textCheck}
-                                        onPress={() => Linking.openURL('https://www.boe.es/eli/es/lo/2018/12/05/3/con')}>
+                                          onPress={() => Linking.openURL('https://www.boe.es/eli/es/lo/2018/12/05/3/con')}>
                                         Aceptar términos y condiciones
-                                </Text>}
+                                    </Text>}
                             />
                         </View>
                         <View style={styles.button}>
-                            <GenericButton button="Create Account" press={this.addUser} />
+                            <GenericButton button="Create Account" press={this.addUser}/>
                         </View>
                     </ScrollView>
                 </View>
