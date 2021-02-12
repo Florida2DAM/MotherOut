ae2bb852afd973b1a34105c3b6b9e78021a0ebd7
Ferriol-blip <70396199+Ferriol-blip@users.noreply.github.com>
2021-02-12 21:27:46 +0100

    Terminada funcionalidades de la ventana de user

--- BackOffice/MotherOut/src/Views/UserView.js
+++ BackOffice/MotherOut/src/Views/UserView.js
@@ -11,7 +11,6 @@ import {TabPanel, TabView} from "primereact/tabview";
 import {RefreshButton} from '../Components/RefreshButton'
 import {DeleteButton} from '../Components/DeleteButton'
 import {MotherOutInputText} from "../Components/MotherOutInputText";
-import {MotherOutButton} from "../Components/MotherOutButton";
 import {Toast} from "primereact/toast";
 
 export class UserView extends Component {
@@ -21,15 +20,10 @@ export class UserView extends Component {
             tableValues: [],
             //Search
             searchValueName: '',
-            searchValueSurname: '',
+            searchValueAsignedTeam: '',
             searchValueEmail: '',
             //Delete
             deleteValue: '',
-            //Reset Pass
-            emailValue: '',
-            oldPassValue: '',
-            newPassValue: '',
-            confirmPassValue: '',
         }
         this.myToast = React.createRef()
     }
@@ -44,49 +38,36 @@ export class UserView extends Component {
                 <Toast ref={this.myToast}/>
                 <TabView activeIndex={this.state.activeIndex}
                          onTabChange={(e) => this.setState({activeIndex: e.index})}>
-                    <TabPanel header='Filtrar' leftIcon='pi pi-filter'>
+                    <TabPanel header='Filter' leftIcon='pi pi-filter'>
                         <div className='p-grid p-fluid'>
                             <RefreshButton onClick={this.getUsers}/>
                             <MotherOutInputText onChange={this.changeValueEmail} value={this.state.searchValueEmail}
                                                 size={'p-md-3'} name={'Email'}/>
+                            <MotherOutInputText onChange={this.changeValueIdTeam}
+                                                value={this.state.searchValueAsignedTeam}
+                                                size={'p-md-3'} name={'Id Team'}/>
                         </div>
                     </TabPanel>
-                    <TabPanel header='Eliminar' leftIcon='pi pi-trash'>
+                    <TabPanel header='Delete' leftIcon='pi pi-trash'>
                         <div className='p-grid p-fluid'>
+                            <RefreshButton onClick={this.getUsers}/>
                             <DeleteButton onClick={this.delUser}/>
                             <MotherOutInputText onChange={(e) => this.setState({deleteValue: e.target.value})}
                                                 value={this.state.deleteValue}
-                                                size={'p-md-3'} name={'Email'}/>
+                                                size={'p-md-3'} name={'Id User'}/>
                         </div>
-                    </TabPanel>
-                    <TabPanel header='Restablecer clave' leftIcon='pi pi-reply'>
-                        <div className='p-grid p-fluid'>
-                            <MotherOutInputText onChange={(e) => this.setState({emailValue: e.target.value})}
-                                                value={this.state.emailValue}
-                                                size={'p-md-6'} name={'Email'}/>
-                            <MotherOutInputText onChange={(e) => this.setState({oldPassValue: e.target.value})}
-                                                value={this.state.oldPassValue}
-                                                size={'p-md-6'} name={'Contraseña'}/>
-                            <MotherOutInputText onChange={(e) => this.setState({newPassValue: e.target.value})}
-                                                value={this.state.newPassValue}
-                                                size={'p-md-6'} name={'Nueva contraseña'}/>
-                            <MotherOutInputText onChange={(e) => this.setState({confirmPassValue: e.target.value})}
-                                                value={this.state.confirmPassValue}
-                                                size={'p-md-6'} name={'Confirmar contraseña'}/>
-                        </div>
-                        <MotherOutButton label={'Restablecer'} icon={'pi pi-refresh'} onClick={this.resetPass}/>
                     </TabPanel>
                 </TabView>
                 <div className='card'>
                     <DataTable value={this.state.tableValues}>
-                        <Column field='UserId' header='UserId'/>
-                        <Column field='Name' header='Nombre'/>
-                        <Column field='Email' header='Email'/>
-                        <Column field='UserMaster' header='Usuario Master'/>
-                        <Column field='AsignedTeam' header='Id Equipo'/>
-                        <Column field='NTaks' header='Tareas realizadas'/>
-                        <Column field='UserScore' header='Score'/>
-                        <Column field='Help' header='Help'/>
+                        <Column field='UserId' header='UserId' style={{width: '5%'}}/>
+                        <Column field='Name' header='Name' style={{width: '10%'}}/>
+                        <Column field='Email' header='Email' style={{width: '15%'}}/>
+                        <Column field='UserMaster' header='User Master' style={{width: '10%'}}/>
+                        <Column field='AsignedTeam' header='Id Team' style={{width: '10%'}}/>
+                        <Column field='NTaks' header='Tasks Done' style={{width: '10%'}}/>
+                        <Column field='UserScore' header='Score' style={{width: '10%'}}/>
+                        <Column field='Help' header='Help' style={{width: '10%'}}/>
                     </DataTable>
                 </div>
             </>
@@ -97,7 +78,13 @@ export class UserView extends Component {
     changeValueEmail = (event) => {
         this.setState({searchValueEmail: event.target.value}, () => {
             this.state.searchValueEmail !== '' ? this.filterByEmail() : this.getUsers();
-            //this.setState({searchValueName: '', searchValueSurname: ''})
+            this.setState({searchValueAsignedTeam: ''})
+        })
+    }
+    changeValueIdTeam = (event) => {
+        this.setState({searchValueAsignedTeam: event.target.value}, () => {
+            this.state.searchValueAsignedTeam !== '' ? this.filterByIdTeam() : this.getUsers();
+            this.setState({searchValueEmail: ''})
         })
     }
 
@@ -108,11 +95,15 @@ export class UserView extends Component {
     filterByEmail = () => {
         this.axiosRequestArray('http://52.0.146.162:80/api/Users?email=' + this.state.searchValueEmail);
     }
+    filterByIdTeam = () => {
+        console.log(this.state.searchValueAsignedTeam)
+        this.axiosRequest('http://52.0.146.162:80/api/Users?idTeam=' + this.state.searchValueAsignedTeam);
+    }
     delUser = () => {
         if (this.state.deleteValue !== '') {
-            this.advAxiosRequest('http://52.0.146.162:80/api/Users?email=' + this.state.deleteValue, 'https://localhost:44390/api/Usuarios', 'User deleted', 'delete')
+            this.advAxiosRequest('http://52.0.146.162:80/api/Users?idUser=' + this.state.deleteValue, 'http://52.0.146.162:80/api/Users', 'User deleted', 'delete')
         } else {
-            this.showToast('warn', 'Campos Vacíos')
+            this.showToast('warn', 'Empty Fields')
         }
         this.setState({deleteValue: ''});
     }
@@ -129,7 +120,7 @@ export class UserView extends Component {
         axios.get(url).then((response) => {
             let array = [];
             array.push(response.data)
-            //this.changeFormatArray(response);
+            this.changeFormatArray(array);
             this.setState({tableValues: array});
         })
     }
@@ -154,7 +145,7 @@ export class UserView extends Component {
             }
         }).catch((err) => {
             if (err != null) {
-                this.showToast('error', 'Request Error');
+                this.showToast('error', 'Server Error');
             }
         });
     }
@@ -165,15 +156,12 @@ export class UserView extends Component {
     //data table format
     changeFormat = (response) => {
         response.data.forEach(u => u.Help ? u.Help = 'Show' : u.Help = 'Hidden');
+        response.data.forEach(u => u.UserMaster ? u.UserMaster = 'Yes' : u.UserMaster = 'No');
     }
-    changeFormatArray = (array) => {
-        array.forEach(u => u.Help ? u.Help = 'Show' : u.Help = 'Hidden');
+    changeFormatArray = (arr) => {
+        console.log(arr)
+        arr.forEach(u => u.UserMaster ? u.UserMaster = 'Yes' : u.UserMaster = 'No');
+        arr.forEach(u => u.Help ? u.Help = 'Show' : u.Help = 'Hidden');
     }
-
-    //Codigo reserva
-    //   <MotherOutInputText onChange={this.changeValueName} value={this.state.searchValueName}
-    //                        size={'p-md-3'} name={'Nombre'}/>
-    //    <MotherOutInputText onChange={this.changeValueSurname} value={this.state.searchValueSurname}
-    //                        size={'p-md-3'} name={'Apellidos'}/>
 }
 
